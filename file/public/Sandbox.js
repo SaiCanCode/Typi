819a457db145daa5fa97fa2d2ca4db9e4de4088d
samuel aderemi ige <123731903+SaiCanCode@users.noreply.github.com>
2025-03-11 08:06:15 +0100

    commit

---
+++ file/public/Sandbox.js
@@ -0,0 +1,11 @@
+"use strict";
+// let ninja : {
+//     age : number,
+//     name : string,
+//     isLoggedin : boolean
+// }
+// ninja = {age: 40, name: "sai", isLoggedin: true}
+let jinx = [];
+jinx.push(6);
+jinx.push('hi');
+console.log(jinx);
